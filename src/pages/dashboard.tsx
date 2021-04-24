0f0e8903eacfe9d0ae9f1641a7d8353ecd4aa1cd
Alexandre Menezes <amenezes031@gmail.com>
2021-04-24 15:22:45 -0300

    Component: Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,16 @@
+import { Flex } from '@chakra-ui/react';
+
 import Header from "../components/Header";
+import Sidebar from '../components/Sidebar';
 
 export default function dashboard() {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" px="6" mx="auto" my="6" maxWidth={1480}>
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
