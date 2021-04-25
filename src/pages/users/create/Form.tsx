cc9d622151d6c40d7ee0ca8f5eca19dd9f66156a
Alexandre Menezes <amenezes031@gmail.com>
2021-04-25 18:30:17 -0300

    Responsive: Register

--- src/pages/users/create/Form.tsx
+++ src/pages/users/create/Form.tsx
@@ -6,12 +6,12 @@ export default function Form() {
   return (
     <>
       <VStack spacing="8">
-        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
           <Input name="name" label="Nome completo" />
           <Input name="email" type="email" label="E-mail" />
         </SimpleGrid>
 
-        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
           <Input name="password" type="password" label="Senha" />
           <Input name="password_confirmation" type="password" label="Confirmação da senha" />
         </SimpleGrid>
--- src/pages/users/create/index.tsx
+++ src/pages/users/create/index.tsx
@@ -14,7 +14,7 @@ export default function CreateUser() {
       <Flex w="100%" px="6" mx="auto" my="6" maxWidth={1480}>
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Heading>Criar usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
