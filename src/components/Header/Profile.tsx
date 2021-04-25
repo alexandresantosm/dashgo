0c692ff624ea272adfbe8a67dfd6168cc5261697
Alexandre Menezes <amenezes031@gmail.com>
2021-04-25 14:33:04 -0300

    Responsive: Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,21 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export default function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Alexandre Santos</Text>
-        
-        <Text color="gray.300" fontSize="small">
-          menezesalexandre@rocketmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Alexandre Santos</Text>
+          
+          <Text color="gray.300" fontSize="small">
+            menezesalexandre@rocketmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Alexandre Santos" src="http://github.com/alexandresantosm.png" />
     </Flex>
