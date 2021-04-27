320789dd597a62032a0e10e679bb684c9eb29b89
Alexandre Menezes <amenezes031@gmail.com>
2021-04-26 22:20:43 -0300

    User create form

--- src/pages/users/create/index.tsx
+++ src/pages/users/create/index.tsx
@@ -1,7 +1,6 @@
-import { Box, Divider, Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
 import Header from '../../../components/Header';
-import Heading from '../../../components/Heading';
 import Sidebar from '../../../components/Sidebar';
 
 import Form from './Form';
@@ -10,17 +9,9 @@ export default function CreateUser() {
   return (
     <Box>
       <Header />
-
       <Flex w="100%" px="6" mx="auto" my="6" maxWidth={1480}>
         <Sidebar />
-
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
-          <Heading>Criar usuário</Heading>
-
-          <Divider my="6" borderColor="gray.700" />
-
-          <Form />
-        </Box>
+        <Form />
       </Flex>
     </Box>
   );
