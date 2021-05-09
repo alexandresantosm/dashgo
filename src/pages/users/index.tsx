5e23bfb762e40e84b483f9be13b1ad6272fbd5e1
Alexandre Menezes <amenezes031@gmail.com>
2021-05-09 18:33:34 -0300

    Replace useQuery with hook useUsers

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,26 +17,7 @@ type User = {
 }
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get('/users');
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        }),
-      }
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5, // 5 segundos
-  });
+  const { data, isLoading, isFetching, error } = useUsers();
 
   return (
     <Box>
